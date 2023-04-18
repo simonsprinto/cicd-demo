106809b38a09654c9fb0196bb396742c6a3a3770
simonsprinto <simon.andersson@sprinto.se>
2023-04-18 16:06:44 +0200

    added headless option

--- src/test/java/com/example/seleniumdemoapp/SeleniumTests.java
+++ src/test/java/com/example/seleniumdemoapp/SeleniumTests.java
@@ -31,6 +31,7 @@ public class SeleniumTests {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("incognito");
+        options.addArguments("headless");
         driver = new ChromeDriver(options);
     }
 
